a6926cccff4a9afaf92fe6e0ff5b4de0a939416d
Andrey <80448375+AndreyLeonidovich@users.noreply.github.com>
2025-08-07 20:34:19 +0300

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -1,2 +1,18 @@
-apt install docker.io docker-compose -y
-systemctl enable docker
+sudo apt update && apt upgrade -y
+sudo adduser bitwarden
+sudo passwd bitwarden
+sudo groupadd docker
+sudo usermod -aG docker bitwarden
+sudo mkdir /opt/bitwarden
+sudo chmod -R 700 /opt/bitwarden
+sudo chown -R bitwarden:bitwarden /opt/bitwarden
+
+
+
+
+
+
+
+
+#apt install docker.io docker-compose -y
+#systemctl enable docker
